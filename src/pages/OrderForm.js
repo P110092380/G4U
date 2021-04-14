5e397212e0de873c16af990f7c675399e31356e0
P110092380 <p110092380@student.peterborough.ac.uk>
2021-04-14 11:51:21 +0100

    Changes made to form

--- src/pages/OrderForm.js
+++ src/pages/OrderForm.js
@@ -10,7 +10,11 @@ class Login extends React.Component {
         eID: '',
         eEmail: '',
         date:'',
-        items:'',
+        pSID:'',
+        pPrice:'',
+        shipping:'',
+        pQuantity:'',
+        bID:'',
       };
     }
     handleFormSubmit(event) {
@@ -28,10 +32,20 @@ class Login extends React.Component {
                     <input type="formText" id="eID" name="eID" value={this.state.eID} onChange={e => this.setState({eID:e.target.value})} />
                     <label className="subtitle">Email</label>
                     <input type="formEmail" id="eEmail" name="eEmail" value={this.state.eEmail} onChange={e => this.setState({eEmail:e.target.value})} />
-                    <label className="subtitle">Date</label>
-                    <input type="formDate" id="Ddte" name="date" value={this.state.Date} onChange={e => this.setState({date :e.target.value})} />
-                    <label className="subtitle">Items</label>
-                    <textarea type="formItems" id="items" name="items" value={this.state.items} onChange={e => this.setState({items:e.target.value})} />
+                    <label className="subtitle">Order date</label>
+                    <input type="formDate" id="Date" name="date" value={this.state.Date} onChange={e => this.setState({date :e.target.value})} />
+                    <form action="#">
+                      <label className="subtitle">Product Supplier ID</label>
+                      <input type="formText" id="pSID" name="pSID" value={this.state.pSID} onChange={e => this.setState({pSID:e.target.value})} />
+                      <label className="subtitle">Product price</label>
+                      <input type="formText" id="pPrice" name="pPrice" value={this.state.pPrice} onChange={e => this.setState({pPrice:e.target.value})} />
+                      <label className="subtitle">Shipping days</label>
+                      <input type="formText" id="shipping" name="shipping" value={this.state.eID} onChange={e => this.setState({shipping:e.target.value})} />
+                      <label className="subtitle">Product quantity</label>
+                      <input type="formText" id="pQuantity" name="pQuantity" value={this.state.pQuantity} onChange={e => this.setState({pQuantity:e.target.value})} />
+                      <label className="subtitle">Brranch ID</label>
+                      <input type="formText" id="bID" name="bID" value={this.state.eID} onChange={e => this.setState({bID:e.target.value})} />
+                    </form>
                     <Link to="/OrderRequest" className="btn btn-primary" role="button">Submit order</Link>
                 </form>
           </div>
